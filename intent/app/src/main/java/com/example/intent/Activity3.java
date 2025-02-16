24116eae90ff98251a0a67ab3f1b06ed7f354dc7
Purvi Porwal <purviporwal1812@gmail.com>
2025-02-16 20:10:15 +0530

    intent

---
+++ intent/app/src/main/java/com/example/intent/Activity3.java
@@ -0,0 +1,23 @@
+package com.example.intent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Activity3 extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity2);
+
+        TextView resultText = findViewById(R.id.result_text);
+
+        Intent intent = getIntent();
+        int counterValue = intent.getIntExtra("counter_value", 0);
+
+        resultText.setText("Counter Value: " + counterValue);
+    }
+}
